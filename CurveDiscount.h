f687e33c5322b5c0eed5485ee53ee6fc71b9746c
gitgjc <74197331+gitgjc@users.noreply.github.com>
2020-11-19 20:00:05 +0800

    Update CurveDiscount.h

--- CurveDiscount.h
+++ CurveDiscount.h
@@ -1,8 +1,8 @@
 #pragma once
-
 #include <vector>
-
 #include "ICurve.h"
+
+//using std::vector;
 
 namespace minirisk {
 
@@ -14,20 +14,23 @@ struct CurveDiscount : ICurveDiscount
 
     CurveDiscount(Market *mkt, const Date& today, const string& curve_name);
 
-    void init_log_discounting_factors(Market *mkt);
+    void local_rates(Market* mkt);
 
     // compute the discount factor
     double df(const Date& t) const;
 
     virtual Date today() const { return m_today; }
 
-private:
-    int32_t convert_tenor_to_int(std::string& tenor) const;
+    int DWMY_days(std::string dwmy);
 
+private:
+    
     Date m_today;
-    Date m_last_tenor_date;
+    //Date m_last_tenor_date;
+    Date dwmy_date;
     string m_name;
-    std::vector<std::pair<double, double>> m_log_dfs;
+    std::vector<std::pair<double, double>> log_dfs;
+    //std::vector< std::pair<double, double> > log_dfs;
     double m_rate;
 };
 
